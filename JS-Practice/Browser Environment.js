485f949d1c6dc3f96e8783527c194ae53bec4870
Om-sachania <ceomsachania.gmail.com>
2024-07-20 14:50:16 +0530

    JavaScript Practice Folder

---
+++ JS-Practice/Browser Environment.js
@@ -0,0 +1,16 @@
+function sayHi(){
+    console.log("Hello");
+}
+
+// console.log(window.innerHeight);
+// console.log(window.innerWidth);
+
+document.body.style.backgroundColor = "lime";
+setTimeout(()=>document.body.style.backgroundColor ="white",2000)
+
+// Browser Object Model : 
+console.log(location.href);
+// if(confirm("Open Youtube?")){
+//     location.href="https://youtube.com";
+// }
+console.dir(document);  
