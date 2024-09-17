aed934fcf4e2323686dac9ba5b0e9a184cdb4644
Om-sachania <ceomsachania@gmail.com>
2024-09-17 11:00:39 +0530

    Delete JS-Practice directory(old one)

--- JS-Practice/ArrayMethods/ForEach.js
+++
@@ -1,5 +0,0 @@
-// Iterate : forEach()
-    // Syntax : arr.forEach(item,index,array)
-    fruits.forEach((item,index,array)=>{
-        console.log(`${item} is at index ${index} in an ${array} array`);
-    });
--- JS-Practice/Hoisting.js
+++
@@ -1,9 +0,0 @@
-let x=5;
-
-function getName() {
-    console.log("Hello World");
-}
-
-getName();
-console.log(x);
-console.log(getName);
